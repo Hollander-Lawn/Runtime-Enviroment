8e7a8ce0f7261bda9adc2b3bf64fb899d79895c3
Hollander-Lawn <hoonyikonnalexander@outlook.com>
2024-07-17 14:50:26 +0800

    Add files via upload

---
+++ Runtime Enviroment.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+class runtime_main {
+	public:
+		void runtime_data() {
+			cout << "runtime initiate\n";
+			int runtime_loading_time;
+			int loader_file_integer;
+			string game_being_loaded;
+			string loading_request;
+
+		}
+};
+
+int main() {
+	runtime_main runtime_obj;
+	runtime_obj.runtime_data();
+
+}
